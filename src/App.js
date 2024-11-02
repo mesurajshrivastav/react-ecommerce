c5b9d7791e0dd4f01a8ee8831ad1031df3096c81
Suraj Shrivastav <109119656+mesurajshrivastav@users.noreply.github.com>
2024-11-02 09:29:28 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -2,11 +2,35 @@ import React from "react";
 import logo from "./logo.svg";
 import ProductList from "./features/product-list/productList";
 import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage/>,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage/>,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <RouterProvider router={router} />
     </div>
   );
 }
