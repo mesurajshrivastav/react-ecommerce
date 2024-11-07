68ed77d5e6b60189f1542a16fbae584f4c4e6981
Suraj Shrivastav <109119656+mesurajshrivastav@users.noreply.github.com>
2024-11-07 23:34:41 +0530

    updated code

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -1,7 +1,7 @@
 // A mock function to mimic making an async request for data
 export function fetchAllProducts() {
   return new Promise(async (resolve) => {
-    //TODO: we will not hard-core server url here
+    //TODO: we will not hard-core server url here !
     const response = await fetch("http://localhost:8080/products");
     const data = await response.json();
     resolve({ data });
